a001044e83ff39f290b46effef309b7a13565779
CarryAllen <rohanshingade1095@gmail.com>
2023-04-15 19:59:54 +0530

    code modified

--- expense-tracker/src/Components/ExpenseItem.js
+++ expense-tracker/src/Components/ExpenseItem.js
@@ -2,12 +2,15 @@ import React from 'react'
 import './ExpenseItem.css';
 
 export default function ExpenseItem() {
+	const expenseDate = new Date(2021, 2, 28);
+	const expenseTitle = 'Car Insurance';
+	const expenseAmount = 294.67;
 	return (
 		<div className='expense-item'>
-			<div> March 28th 2021 </div>
+			<div> {expenseDate.toISOString()} </div>
 			<div className='expense-item__description'>
-				<h2> Car insurance </h2>
-				<div className='expense-item__price'> 25000/- </div>
+				<h2> {expenseTitle} </h2>
+				<div className='expense-item__price'> {expenseAmount} </div>
 			</div>
 		</div>
 	)
