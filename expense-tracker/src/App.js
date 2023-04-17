e70ff64bf5d0e9d502f9d9033581cf7e1e1f07aa
CarryAllen <rohanshingade1095@gmail.com>
2023-04-17 19:16:37 +0530

    code modified

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import ExpenseItem from './Components/ExpenseItem';
 
 function App() {
+  const expensesArray = [ //array to pass item to componet mentioned as props
+
+      {
+        id:"e1",
+        title: 'Car Insureance', 
+        amount: 294.67, 
+        date: new Date(2021,5,12),  
+      },
+      {
+        id:"e2",
+        title: 'Sofa repair', 
+        amount: 200.83, 
+        date: new Date(2021,5,28),  
+      },
+      {
+        id:"e3",
+        title: 'New TV', 
+        amount: 294.67, 
+        date: new Date(2021,5,29),  
+      }
+  ];
   return (
     <div>
       <h1> Hello, this is React </h1>
-      <ExpenseItem/>
+
+      <ExpenseItem title={expensesArray[0].title} amount={expensesArray[0].amount} date={expensesArray[0].date} /> 
+      <ExpenseItem title={expensesArray[1].title} amount={expensesArray[1].amount} date={expensesArray[1].date} /> 
+      <ExpenseItem title={expensesArray[2].title} amount={expensesArray[2].amount} date={expensesArray[3].date} /> 
+      
     </div>
   );
 }
@@ -14,25 +39,3 @@ function App() {
 export default App;
 
 
-
-
-/* 
-
-<div className="App">
-  <header className="App-header">
-    <img src={logo} className="App-logo" alt="logo" />
-    <p>
-      Edit <code>src/App.js</code> and save to reload.
-    </p>
-    <a
-      className="App-link"
-      href="https://reactjs.org"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      Learn React
-    </a>
-  </header>
-</div>
-
-*/
