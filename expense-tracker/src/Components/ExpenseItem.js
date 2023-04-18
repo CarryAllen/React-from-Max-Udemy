57919610591ac7c4cbfffcbb4d989e78da3bfc5e
CarryAllen <rohanshingade1095@gmail.com>
2023-04-18 17:30:48 +0530

    code modified

--- expense-tracker/src/Components/ExpenseItem.js
+++ expense-tracker/src/Components/ExpenseItem.js
@@ -11,6 +11,7 @@ export default function ExpenseItem(props) {
 		<div className='expense-item'>
 			<ExpenseDate date={props.date}/>
 			<div className='expense-item__description'>
+				
 				<h2> {props.title} </h2>
 				<div className='expense-item__price'> ${props.amount} </div>
 			</div>
