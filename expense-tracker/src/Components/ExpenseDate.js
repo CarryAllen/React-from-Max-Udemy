3c31f43aa4deb80b66f1e4601f79e7a63054f68f
CarryAllen <rohanshingade1095@gmail.com>
2023-04-18 12:27:27 +0530

    ExpenseDate component done

--- expense-tracker/src/Components/ExpenseDate.js
+++ expense-tracker/src/Components/ExpenseDate.js
@@ -1,3 +1,5 @@
+import './ExpenseDate.css'
+
 export default function ExpenseDate (props) {
 
   const month = props.date.toLocaleString('en-US',{month: 'long'});
@@ -5,10 +7,10 @@ export default function ExpenseDate (props) {
   const year = props.date.getFullYear();
 
   return (
-    <div>
-    <div>{month}</div>
-    <div>{year}</div>
-    <div>{day}</div>
-  </div>
+    <div className='expense-date'>
+      <div className='expense-date__month'>{month}</div>
+      <div className='expense-date__year'>{year}</div>
+      <div className='expense-date__day'>{day}</div>
+    </div>
   );
 }
--- expense-tracker/src/Components/ExpenseItem.js
+++ expense-tracker/src/Components/ExpenseItem.js
@@ -5,11 +5,11 @@ import ExpenseDate from './ExpenseDate';
 
 export default function ExpenseItem(props) {
 
-	<ExpenseDate/>
+	
 
 	return (
 		<div className='expense-item'>
-			
+			<ExpenseDate date={props.date}/>
 			<div className='expense-item__description'>
 				<h2> {props.title} </h2>
 				<div className='expense-item__price'> ${props.amount} </div>
