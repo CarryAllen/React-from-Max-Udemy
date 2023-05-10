6344b652b09724ac57b2d03fb4e970b368cddd93
CarryAllen <rohanshingade1095@gmail.com>
2023-05-10 16:11:45 +0530

    clear fields after click add expenses

--- expense-tracker/src/Components/NewExpense/ExpenseForm.js
+++ expense-tracker/src/Components/NewExpense/ExpenseForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import './ExpenseForm.css'
 
+
+
 const ExpenseForm = () => {
 
   const [enteredTitle, setEnteredTitle] = useState(''); //format of Array destructuring
@@ -65,6 +67,9 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
 
   return (
@@ -72,17 +77,17 @@ const ExpenseForm = () => {
     <div className='new-expense__controls'>
       <div className='new-expense__control'>
         <label>Title</label>
-        <input type='text' onChange={titleChangeHandler} />
+        <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
       </div>
 
       <div className='new-expense__control'>
         <label>Amount</label>
-        <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler} />
+        <input type='number' min="0.01" step="0.01" value={amount} onChange={amountChangeHandler} />
       </div>
 
       <div className='new-expense__control'>
         <label>Date</label>
-        <input type='date' min="2019-01-01" step="2022-12-31" onChange={dateChangeHandler} />
+        <input type='date' min="2019-01-01" step="2022-12-31" value={date} onChange={dateChangeHandler} />
       </div>
     </div>
 
