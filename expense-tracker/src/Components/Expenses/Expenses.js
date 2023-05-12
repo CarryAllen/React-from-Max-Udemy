a89911094d89fe8d41a0b2b185d5ecae42abe5a7
CarryAllen <rohanshingade1095@gmail.com>
2023-05-12 11:07:30 +0530

    ..

--- expense-tracker/src/Components/Expenses/Expenses.js
+++ expense-tracker/src/Components/Expenses/Expenses.js
@@ -14,6 +14,7 @@ const Expenses = (props) => {
   };
 
   return (
+
     <div>
 
       <Card className='expenses'>
@@ -23,7 +24,7 @@ const Expenses = (props) => {
         {props.items.map(expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>)} 
         {/* here "expense" is an object of map array function function thrrough which we are accessing values */}
         
-        
+
         {/* <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} /> 
 
         <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} /> 
@@ -35,6 +36,7 @@ const Expenses = (props) => {
       </Card>
 
     </div>
+    
   )
 }
 
