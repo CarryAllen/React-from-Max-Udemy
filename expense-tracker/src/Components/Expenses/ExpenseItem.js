cbfbe8d13321f599d23794b21f27f14cbc635e5a
CarryAllen <rohanshingade1095@gmail.com>
2023-05-12 11:04:48 +0530

    add new expense to the list

--- expense-tracker/src/Components/Expenses/ExpenseItem.js
+++ expense-tracker/src/Components/Expenses/ExpenseItem.js
@@ -6,14 +6,14 @@ import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
 
-	const [title, setTitle] = useState(props.title);		//A typ of React Hook		//herr props.title represents the initial state
+	const [title, setTitle] = useState(props.title);		//A type of React Hook		//herr props.title represents the initial state
 	
-
+/* 
 	const clickHandler = () => {
 		setTitle('Updated');
 		console.log(title);
 	};
-
+ */
 	
 	return (
 		<Card className='expense-item'>
@@ -22,7 +22,7 @@ const ExpenseItem = (props) => {
 				<h2> {title} </h2>
 				<div className='expense-item__price'> ${props.amount} </div>
 			</div>
-			<button onClick={clickHandler}> lets change it </button>
+			{/* <button onClick={clickHandler}> lets change it </button> */}
 		</Card>
 	)
 }
