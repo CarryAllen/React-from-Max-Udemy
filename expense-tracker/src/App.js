2ee379e779d22a7898e125c736a6295da33d2ea6
CarryAllen <rohanshingade1095@gmail.com>
2023-05-13 11:31:46 +0530

    error resolved

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -37,7 +37,7 @@ const DUMMY_EXPENSES = [
 
 const App = () => {
 
-  const [expenses, setExpenses] = useState('DUMMY_EXPENSES');
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {  
     setExpenses((prevExpenses) => {
