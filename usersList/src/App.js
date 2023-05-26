c72a225a7791255a3bd5be961ac32e4f17895158
CarryAllen <rohanshingade1095@gmail.com>
2023-05-26 17:32:15 +0530

    AddUaser & Card components added

--- usersList/src/App.js
+++ usersList/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import AddUser from './Components/Users/AddUser';
+import './Components/Users/AddUser.css';
 
 function App() {
   return (
     <div>
-
+      <AddUser/>
     </div>
   );
 }
