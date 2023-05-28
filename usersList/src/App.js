66880dee773e8a9211503cb3e9062720da0f5434
CarryAllen <rohanshingade1095@gmail.com>
2023-05-28 12:52:20 +0530

    code edited for users list

--- usersList/src/App.js
+++ usersList/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [userslist, setUsersList] = useState([]);
 
   const addUserHandler = (uName, uAge) => {
-    setUsersList((...prevUsersList) => {
+    setUsersList((prevUsersList) => {
       return [
         ...prevUsersList, 
         {id: Math.random().toString(), name:uName, age:uAge }
